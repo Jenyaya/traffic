93e29a39c0d8c251839c03a5b14e164658cbb28e
Jenyaya <jenyaya@online.ua>
2012-12-25 12:43:45 +0000

    size changed

--- features/step_definitions/browser_steps.rb
+++ features/step_definitions/browser_steps.rb
@@ -1,7 +1,7 @@
 Given /^opened "(.*?)" page$/ do |page|
   case page
     when 'Yandex Maps' then
-      page = 'http://maps.yandex.ua/?ll=30.582463%2C50.439321&spn=0.494385%2C0.140106&z=12&l=map%2Ctrf%2Ccmr%2Cmap&trfm=cur'
+      page = 'http://maps.yandex.ua/?ll=30.570627%2C50.457148&spn=0.988770%2C0.309476&z=11&l=map%2Ctrf%2Ccmr%2Cmap&trfm=cur'
   end
   visit page
 

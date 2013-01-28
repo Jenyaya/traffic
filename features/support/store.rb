e36a6fe8cf7558e4716959a8217e7d697b50fbe8
Jenyaya <jenyaya@online.ua>
2013-01-28 13:57:02 +0000

    aero added

--- features/support/store.rb
+++ features/support/store.rb
@@ -2,14 +2,8 @@ require 'csv'
 
 def save_flight_details(flight_info)
 
- # depart_time = CSV.parse(depart_time)
-
-  CSV.open("depart.csv", "a") do |csv|
+  CSV.open("C:/screens/depart.csv", "a") do |csv|
     csv << flight_info
-
   end
 
-
 end
-
-#save_flight_details([Time.now, 'asfas'])
